b59aa71f5f6f02ddd19208a1b8cba3ebe480b3b5
Cits123 <101889069+Cits123@users.noreply.github.com>
2022-04-24 14:08:21 +0530

    commit

--- src/main/java/com/bezkoder/spring/login/repository/tutorialrepository.java
+++ src/main/java/com/bezkoder/spring/login/repository/tutorialrepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface tutorialrepository extends JpaRepository<Tutorial, Integer> {
     Object findAll = null;
-    List<Tutorial> findByPublishe(String published);
+    List<Tutorial> findByPublished(String published);
     static List<Tutorial> findByTitleContaining(String title) {
       // TODO Auto-generated method stub
       return null;
